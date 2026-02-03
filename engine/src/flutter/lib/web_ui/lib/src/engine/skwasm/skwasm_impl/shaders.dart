06d28dafdb178b9a5b3839cc632efb732877c3cd
Evan Walley <evanwall@buffalo.edu>
2026-02-03 11:36:58 -0800

    update web ui fragment shader tests

--- engine/src/flutter/lib/web_ui/lib/src/engine/skwasm/skwasm_impl/shaders.dart
+++ engine/src/flutter/lib/web_ui/lib/src/engine/skwasm/skwasm_impl/shaders.dart
@@ -376,7 +376,7 @@ class SkwasmFragmentShader implements SkwasmShader, ui.FragmentShader {
 
     IndexError.check(index, info.floatCount, message: 'Index `$index` out of bounds for `$name`.');
 
-    return SkwasmUniformFloatSlot._(this, index, name, info.location + index);
+    return SkwasmUniformFloatSlot._(this, index, name, info.floatOffset + index);
   }
 
   @override
@@ -411,7 +411,7 @@ class SkwasmFragmentShader implements SkwasmShader, ui.FragmentShader {
 
     return List<SkwasmUniformFloatSlot>.generate(
       size,
-      (i) => SkwasmUniformFloatSlot._(this, i, name, info.location + i),
+      (i) => SkwasmUniformFloatSlot._(this, i, name, info.floatOffset + i),
     );
   }
 
@@ -432,7 +432,7 @@ class SkwasmFragmentShader implements SkwasmShader, ui.FragmentShader {
     final elements = List<T>.generate(numElements, (i) {
       final slots = List<SkwasmUniformFloatSlot>.generate(
         info.floatCount,
-        (j) => SkwasmUniformFloatSlot._(this, j, name, info.location + i * elementSize + j),
+        (j) => SkwasmUniformFloatSlot._(this, j, name, info.floatOffset + i * elementSize + j),
       );
       return elementFactory(slots);
     });
